2f747d49e286f45c6aef8c8898ca7daf2583433a
naram.dash <juho_kim@outlook.com>
2021-05-03 05:33:36 +0900

    applying react-helmet with plugin

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,10 +1,14 @@
 import React from "react"
 import { graphql, PageProps } from "gatsby"
+import { Helmet } from "react-helmet"
 
 export default function BlogPost(props: PageProps & { data: Data }) {
   const post = props.data.markdownRemark
   return (
     <div>
+      <Helmet>
+        <title>{post.frontmatter.title} | KimJuho's blog</title>
+      </Helmet>
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
