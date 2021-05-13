b2d02cd6f1a175c11b7469aac65a2b17407f77a2
naram.dash <juho_kim@outlook.com>
2021-05-13 13:16:15 +0900

    Header a to Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,17 @@
+import { Link } from "gatsby"
 import React from "react"
 
 export default function Header() {
   return (
     <nav className="p-3 d-flex flex-row flex-items-center flex-justify-between color-bg-info">
-      <a href="/">
+      <Link to="/">
         <h1
           className="h3 color-text-primary"
           style={{ fontFamily: "'Nanum Gothic Coding', monospace" }}
         >
           blog.juho.kim
         </h1>
-      </a>
+      </Link>
     </nav>
   )
 }
