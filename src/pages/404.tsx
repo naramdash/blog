371a25a9b6151f7caf787f29e45e7c0000217b33
naram.dash <juho_kim@outlook.com>
2021-05-15 12:31:00 +0900

    minimize usign @primer/components

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link, PageProps } from "gatsby"
-import { Flex, Link as PrimerLink } from "@primer/components"
 
 export default function NotFound(props: PageProps) {
   return (
@@ -23,12 +22,10 @@ export default function NotFound(props: PageProps) {
 `}
       </pre>
 
-      <Flex flexDirection="column" style={{ gap: "10px" }}>
+      <div className="d-flex flex-column" style={{ gap: "10px" }}>
         <Link to="/">홈으로 가기</Link>
-        <PrimerLink as="a" onClick={() => history.back()}>
-          뒤로 가기
-        </PrimerLink>
-      </Flex>
+        <a onClick={() => history.back()}>뒤로 가기</a>
+      </div>
     </div>
   )
 }
