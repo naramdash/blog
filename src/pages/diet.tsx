95797b35d7fce2600c5ea22091dd23caa8b86d9e
naram.dash <juho_kim@outlook.com>
2021-05-28 12:30:53 +0900

    simple styling on diet page iframe

--- src/pages/diet.tsx
+++ src/pages/diet.tsx
@@ -1,14 +1,12 @@
 import React from "react"
 import Diet from "../consts/Diet"
+import * as style from "./diet.module.css"
 
 export default function diet() {
   return (
     <main>
       <h1>다이어트</h1>
-      <iframe
-        style={{ width: "100%", maxWidth: "1170px", minHeight: "800px" }}
-        src={Diet.menuTableLink}
-      ></iframe>
+      <iframe className={style.menu} src={Diet.menuTableLink}></iframe>
     </main>
   )
 }
