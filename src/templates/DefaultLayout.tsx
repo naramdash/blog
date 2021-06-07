a5b2a8b3e0e422ae831a25062040c4024e2d5714
naram.dash <juho_kim@outlook.com>
2021-06-08 04:40:59 +0900

    footer sticky to bottom

--- src/templates/DefaultLayout.tsx
+++ src/templates/DefaultLayout.tsx
@@ -2,10 +2,11 @@ import React from "react"
 import { PageProps } from "gatsby"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import * as styles from "./DefaultLayout.module.css"
 
 export default function DefaultLayout(props: PageProps) {
   return (
-    <div>
+    <div className={styles.container}>
       <Header location={props.location} />
       {props.children}
       <Footer />
