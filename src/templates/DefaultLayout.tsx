a043ad834c84d5aa8d52cdaeed7fef2759e31c15
naram.dash <juho_kim@outlook.com>
2021-06-10 03:48:22 +0900

    fixing DefaultLayout className & style missing

--- src/templates/DefaultLayout.tsx
+++ src/templates/DefaultLayout.tsx
@@ -3,12 +3,16 @@ import { PageProps } from "gatsby"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 
+const ClassName = "d-flex flex-column"
+const Style = { minHeight: "100vh" }
 export default function DefaultLayout(props: PageProps) {
   return (
-    <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
-      <Header location={props.location} />
-      {props.children}
-      <Footer />
+    <div>
+      <div className={ClassName} style={Style}>
+        <Header location={props.location} />
+        {props.children}
+        <Footer />
+      </div>
     </div>
   )
 }
