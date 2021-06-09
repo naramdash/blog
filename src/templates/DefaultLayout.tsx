9f3ceedc32a34db716d38fb6f6e03c8baf2fb2ec
naram.dash <juho_kim@outlook.com>
2021-06-09 19:51:29 +0900

    change defaultLayout styling method

--- src/templates/DefaultLayout.tsx
+++ src/templates/DefaultLayout.tsx
@@ -2,11 +2,10 @@ import React from "react"
 import { PageProps } from "gatsby"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import * as styles from "./DefaultLayout.module.css"
 
 export default function DefaultLayout(props: PageProps) {
   return (
-    <div className={styles.container}>
+    <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
       <Header location={props.location} />
       {props.children}
       <Footer />
