0883413622d758392d13722a34346bbcbd3af9cb
naram.dash <juho_kim@outlook.com>
2021-06-13 13:38:12 +0900

    add Google Analytics

--- src/html.js
+++ src/html.js
@@ -17,6 +17,22 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-F75XRPDW2K"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-F75XRPDW2K');
+          `,
+          }}
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
