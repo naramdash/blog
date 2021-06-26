9da79751fd6e933f6c62448e0f61e116d4a44745
naram.dash <juho_kim@outlook.com>
2021-06-27 01:30:46 +0900

    Fixed Layout Button on DefaultLayout

---
+++ src/components/FixedButtons.tsx
@@ -0,0 +1,42 @@
+import clsx from "clsx"
+import React, { useEffect, useState } from "react"
+import * as styles from "./FixedButtons.module.css"
+
+type ScrollDirection = "UP" | "DOWN"
+
+function navigateToTop() {
+  document.documentElement.scrollTo({ top: 0, behavior: "smooth" })
+}
+
+export default function FixedButtons() {
+  const [scrollTop, setScrollTop] = useState(0)
+  const [scrollDirection, setScrollDirection] = useState<ScrollDirection>(
+    "DOWN",
+  )
+
+  useEffect(() => {
+    window.onscroll = function () {
+      setScrollTop((prev) => {
+        setScrollDirection(
+          prev < document.documentElement.scrollTop ? "DOWN" : "UP",
+        )
+        return document.documentElement.scrollTop
+      })
+    }
+  }, [])
+  return (
+    <div className={styles.container}>
+      <button
+        type="button"
+        className={clsx(
+          styles.button,
+          styles.top,
+          scrollDirection === "UP" && styles.hidden,
+        )}
+        onClick={navigateToTop}
+      >
+        🔝
+      </button>
+    </div>
+  )
+}
--- src/templates/DefaultLayout.tsx
+++ src/templates/DefaultLayout.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { PageProps } from "gatsby"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import FixedButtons from "../components/FixedButtons"
 
 const ClassName = "d-flex flex-column"
 const Style = { minHeight: "100vh" }
@@ -12,6 +13,7 @@ export default function DefaultLayout(props: PageProps) {
         <Header location={props.location} />
         {props.children}
         <Footer />
+        <FixedButtons />
       </div>
     </div>
   )
