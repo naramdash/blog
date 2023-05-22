986ba9194fd9bd18d6c3267c3736e75cf7f5c7b7
김주호 <juho_kim@outlook.com>
2023-05-22 19:09:52 +0900

    add microsoft clarity

--- src/html.js
+++ src/html.js
@@ -40,6 +40,17 @@ export default function HTML(props) {
           }}
         />
         {/* Google Analytics = END */}
+        {/* Microsoft Clarity = START */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "h7vmh02d8f");
+          `
+        }} />
+        {/* Microsoft Clarity = END */}
         {/* ChannelTalk = START */}
         <script
           dangerouslySetInnerHTML={{
